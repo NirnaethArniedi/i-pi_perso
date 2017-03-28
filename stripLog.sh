df5dd3e908cc3b026d8dabbfd9109cbbd2bfc9b6
NirnaethArniedi <augustin.lenormand_github@polytechnique.org>
2017-03-28 12:39:33 +0200

    Updated version of stripLog.sh

--- stripLog.sh
+++ stripLog.sh
@@ -4,7 +4,7 @@ name=$1
 
 awk '/Force Differe/ {$1=$2=$NF="";$0=$0;$1=$1;print $0}' $name > simu_forcesDiff.out
 awk '/Forces/ {$1=$NF="";$0=$0;$1=$1;print $0}' $name > simu_maxForces.out
-awk '/Energy Diff/ {$1=$2=$NF="";$0=$0;$1=$1;print $0}' $name > simu_energyDiff.out
+awk '/Energy/ {$1=$2=$NF="";$0=$0;$1=$1;print $0}' $name > simu_energyDiff.out
 awk '/osition/ {$1=$NF="";$0=$0;$1=$1;print $0}' $name > simu_positions.out
 
 exit 0
